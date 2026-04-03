6a98c2c12041b51e4258ab1292f6591b0a03e524
Shubham Jani <shubhamjani1731@gmail.com>
2026-04-03 11:21:22 +0530

    Create file_handler.py

---
+++ Backend/Utiles/file_handler.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+def read_json(path):
+    if not os.path.exists(path):
+        return []
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def write_json(path, data):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
